60b49103d9b463aa0d06d48dc04dc925dadcdf29
Amit Singh Tomar <amittomar287@gmail.com>
2015-08-20 11:46:14 +0530

    Twitter fabric

---
+++ TwitterFabricIntegration/gen/com/example/twitterfabricintegration/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.twitterfabricintegration;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
